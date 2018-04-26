2119752ac1c7279df895b73e2d63b54fee1e4ac3
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-04-25 21:24:29 -0700

    Set execute permission on bin.js Rework summary output per package.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,8 @@ require('babel/register')
 var gulp = require('gulp')
 var babel = require('gulp-babel')
 var mocha = require('gulp-mocha')
+var filter = require('gulp-filter')
+var chmod = require('gulp-chmod')
 
 gulp.task('test', ['build'], function () {
   require('should')
@@ -24,8 +26,12 @@ gulp.task('versions', function (cb) {
 })
 
 gulp.task('build', function () {
+  var f = filter('lib/bin.js', {restore: true})
   return gulp.src('lib/**/*.js')
     .pipe(babel())
+    .pipe(f)
+    .pipe(chmod(0o755))
+    .pipe(f.restore)
     .pipe(gulp.dest('dist'))
 })
 
