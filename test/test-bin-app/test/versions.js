287a131f2c2d087fefa09ce3863f7f31081db3a2
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-11-19 05:54:04 -0800

    test/test-bin-app
    create simple app to test bin.js

---
+++ test/test-bin-app/test/versions.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const VS = require('testeachversion/lib/version-spec')
+
+const packages = module.exports = []
+
+test('ap')
+
+//
+// Helpers
+//
+
+function test (name, options = {}) {
+  let ranges
+  if (typeof options === 'string') {
+    ranges = options
+    options = {}
+  } else if (options.ranges) {
+    ranges = options.ranges
+  }
+
+  packages.push(new VS(name, {
+    ranges,
+    task: options.task || 'node index.js',
+  }))
+}
